54d3c48a6613133305a7d1baabc35f98b5f53852
Apache3 <tj.thomas.jeantet@gmail.com>
2017-12-17 03:20:26 +0100

    first commit

---
+++ src/net/shape_net.py
@@ -0,0 +1,83 @@
+import lmdb
+import numpy as np
+import cv2
+import caffe
+from caffe.proto import caffe_pb2
+import matplotlib.pyplot as plt
+import sys
+
+def get_cam_image(cam, mirror=False):
+  	
+	ret_val, img = cam.read()
+	if mirror: 
+		img = cv2.flip(img, 1)
+	return img
+
+def forward_img_to_net(img,net):
+	if img.shape != [28,28,3]:
+			img2 = cv2.resize(img,(28,28))
+			img = img2.reshape(28,28,-1)
+	else:
+		img = img.reshape(28,28,-1)
+
+	res = net.forward(data = np.asarray([img.transpose(2,0,1)]))
+	argmax = res.values()[0]
+	pred = res.values()[1]
+	print label_tab[pred.argmax()]
+	print pred
+
+if len(sys.argv) <2:
+	print 'usage: net_test train/predict + img_filename(s)'
+	sys.exit(0)
+
+solver_prototxt = 'lenet_solver.prototxt'
+predict_prototxt = 'lenet.prototxt'
+caffemodel = '_iter_5000.caffemodel'
+label_tab = ['Cross', 'Diamond', 'Disc', 'Square', 'Triangle', 'Octogon']
+
+if sys.argv[1] == 'train':
+	caffe.set_mode_cpu()
+	solver = caffe.get_solver(solver_prototxt)
+	solver.solve()
+
+elif sys.argv[1] == 'predict' and len(sys.argv)>2:
+	img_filenames=[]
+	for i in list(range(len(sys.argv)-2)):
+		img_filenames.append(sys.argv[2+i])
+		print i
+
+	print img_filenames
+	caffe.set_mode_cpu()
+	net = caffe.Classifier(predict_prototxt, caffemodel,caffe.TEST,raw_scale=255)
+	print "successfully loaded classifier"
+
+	for img_filename in img_filenames:
+		
+		img = cv2.imread(img_filename)
+
+		if img.shape != [28,28,3]:
+			img2 = cv2.resize(img,(28,28))
+			img = img2.reshape(28,28,-1)
+		else:
+			img = img.reshape(28,28,-1)
+
+		res = net.forward(data = np.asarray([img.transpose(2,0,1)]))
+		argmax = res.values()[0]
+		pred = res.values()[1]
+		print label_tab[pred.argmax()]
+		print pred
+elif sys.argv[1] == 'camera':
+	caffe.set_mode_cpu()
+	net = caffe.Classifier(predict_prototxt, caffemodel,caffe.TEST,raw_scale=255)
+	print "successfully loaded classifier"
+	cam = cv2.VideoCapture(0)
+	while True:
+		img = get_cam_image(cam)
+		cv2.imshow('my webcam', img)
+		if cv2.waitKey(1) == 27: 
+			break  # esc to quit
+		forward_img_to_net(img,net)
+		
+else:
+	print 'usage: net_test train/predict  + img_filename'
+	sys.exit(0)

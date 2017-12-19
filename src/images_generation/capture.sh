3782b480d2aafff09ee8c705e0d5b75bc5eb5c26
ElBabar <bainierba@gmail.com>
2017-12-19 20:02:00 +0100

    changed capture script to 96x54 output

--- src/images_generation/capture.sh
+++ src/images_generation/capture.sh
@@ -42,26 +42,26 @@ snap()
 {
 	LADATE=$(date +_%m%d%H%M%S)
 	case $1 in
-		c ) 	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Cross/cross$LADATE
-			convert $ROOT/images/Cross/cross$LADATE -resize 28x28 $ROOT/images/Cross/cross$LADATE
+		c ) 	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Cross/cross$LADATE
+			convert $ROOT/images/Cross/cross$LADATE -resize 96x54 $ROOT/images/Cross/cross$LADATE
 			echo "\033[1Asnap cross$LADATE";;
-		l )	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Diamond/diamond$LADATE
-			convert $ROOT/images/Diamond/diamond$LADATE -resize 28x28 $ROOT/images/Diamond/diamond$LADATE
+		l )	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Diamond/diamond$LADATE
+			convert $ROOT/images/Diamond/diamond$LADATE -resize 96x54 $ROOT/images/Diamond/diamond$LADATE
 			echo "\033[1Asnap diamond$LADATE";;
-		d )	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Disc/disc$LADATE
-			convert $ROOT/images/Disc/disc$LADATE -resize 28x28 $ROOT/images/Disc/disc$LADATE
+		d )	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Disc/disc$LADATE
+			convert $ROOT/images/Disc/disc$LADATE -resize 96x54 $ROOT/images/Disc/disc$LADATE
 			echo "\033[1Asnap disc$LADATE";;
-		s )	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Square/square$LADATE
-			convert $ROOT/images/Square/square$LADATE -resize 28x28 $ROOT/images/Square/square$LADATE
+		s )	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Square/square$LADATE
+			convert $ROOT/images/Square/square$LADATE -resize 96x54 $ROOT/images/Square/square$LADATE
 			echo "\033[1Asnap square$LADATE";;
-		t )	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Triangle/triangle$LADATE
-			convert $ROOT/images/Triangle/triangle$LADATE -resize 28x28 $ROOT/images/Triangle/triangle$LADATE
+		t )	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Triangle/triangle$LADATE
+			convert $ROOT/images/Triangle/triangle$LADATE -resize 96x54 $ROOT/images/Triangle/triangle$LADATE
 			echo "\033[1Asnap triangle$LADATE";;
-		o )	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Octogon/octogon$LADATE
+		o )	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Octogon/octogon$LADATE
 			convert $ROOT/images/Octogon/octogon$LADATE -resize 28x28 $ROOT/images/Octogon/octogon$LADATE
 			echo "\033[1Asnap octogon$LADATE";;
-		b )	raspistill -t 900 -e png -h 720 -w 720 -ex auto -awb auto -o $ROOT/images/Background/background$LADATE
-			convert $ROOT/images/Background/background$LADATE -resize 28x28 $ROOT/images/Background/background$LADATE
+		b )	raspistill -t 900 -e png -h 1080 -w 1920 -ex auto -awb auto -o $ROOT/images/Background/background$LADATE
+			convert $ROOT/images/Background/background$LADATE -resize 96x54 $ROOT/images/Background/background$LADATE
 			echo "\033[1Asnap background$LADATE";;
 	esac
 }

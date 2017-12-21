438a976b54c9b1d19e87445aacd1fa96a77edff0
Apache3 <tj.thomas.jeantet@gmail.com>
2017-12-21 18:07:11 +0100

    adapted for raspberry pi

--- src/camera positionner/positioner.py
+++ src/camera positionner/positioner.py
@@ -41,7 +41,6 @@ for frame in cam.capture_continuous(rawCapture, format="bgr", use_video_port=Tru
 		cv2.imshow("Frame", image)
 		key = cv2.waitKey(1) & 0xFF
 		image = cv2.resize(image,(94,56))
-		test_img(image)
 
 		# clear the stream in preparation or the next frame
 		rawCapture.truncate(0)
